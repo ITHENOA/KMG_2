fc41b1869bd2e05ffe6d9527c9703d1c2b75b36e
HojjatHosseini <79358150+ITHENOA@users.noreply.github.com>
2024-06-14 11:07:36 +0000

    14jan

--- pytest/test_somfnn.py
+++ pytest/test_somfnn.py
@@ -2,16 +2,11 @@ from SOMFNN import SOMFNN
 import torch
 
 # Generate random data
-X = torch.randn(3,20)
-Y = torch.randn(1,20)
+X = torch.randn(3, 20)
+Y = torch.randn(1, 20)
 
 # network
-net = SOMFNN(in_features=3, hidden_features=[3,2], out_features=1)
-net.options(
-    n_epoch=100, 
-    lr=0.01, 
-    criterion='MSE', 
-    optimizer='Adam'
-    )
+net = SOMFNN(in_features=3, hidden_features=[3, 2], out_features=1)
+net.options(n_epoch=100, lr=0.01, criterion="MSE", optimizer="Adam")
 net.trainnet(X, Y)
 net.testnet(X, Y)
