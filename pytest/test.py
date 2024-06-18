b885671197b0344029e918756691ce093dd60f64
HojjatHosseini <79358150+ITHENOA@users.noreply.github.com>
2024-06-18 13:43:24 +0000

    Pending changes exported from your codespace

--- pytest/test.py
+++ pytest/test.py
@@ -1,55 +1,6 @@
-# from typing import Any
-
-
-# class a():
-#     def __init__(v) -> None:
-#         v.a1 = 1
-
-#     def __call__(self,aaa):
-#         print(self.a1)
-#         return aaa
-    
-# class c(a):
-#     def __init__(self) -> None:
-#         a.__init__(self)
-#         print(self.a1)
-#     def cf(self):
-#         return self.a1
-
-# class b(a):
-#     def __init__(self) -> None:
-#         aa = a()
-#         a.__init__(self)
-#         print(self.a1)
-#         self.a1 = 2
-        
-
-# aaa = b()
-
-# a = [4]
-# b = []
-# c = [2,5]
-# print(a+b+c)
-# # from torch import tensor
-# # import torch
-# # x = tensor([[1],[2],[3]])
-# # print(torch.transpose(x,1,0)@x)
-# # print(x**2)
-
-# xk = {
-#     "value": [[1,2,3],[2,3,5]],
-#     "SEN": [2,3],
-# }
-# print(xk)
-# print(xk["value"])
-# print(xk["SEN"])
-import numpy as np
 import torch
 
-
-a = torch.ones(1,3) * 100
-b = torch.randn(5,8)
-c = torch.tensor([1,0,0,1,0]).view(-1,1)
-print(b)
-print(b[[[1],[2],[2],[2],[4],[4],[4],[4]],torch.arange(8)])
-# print(idx)
+a = torch.tensor(1., requires_grad=True)
+b = torch.tensor([],requires_grad=False)
+b = a * 2
+print(a)
---
+++ pytest/train.py
@@ -0,0 +1,24 @@
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import matplotlib.pyplot as plt
+from somfnn import SOMFNN
+
+
+def main():
+    # Generate random data
+    X = torch.randn(100, 4)
+    Y = torch.randn(100, 1)
+
+    # Create dataset and dataloader
+    dataset = TensorDataset(X, Y)
+    dataloader = DataLoader(dataset, batch_size=20, shuffle=True)
+    torch.autograd.set_detect_anomaly(True)
+    # Initialize network
+    net = SOMFNN(in_features=4, hidden_features=[3, 2], out_features=1)
+    net.set_options(num_epochs=100, learning_rate=0.01, criterion="CrossEntropy", optimizer="Adam")
+    net.trainnet(dataloader)
+    net.testnet(dataloader)
+
+
+if __name__ == "__main__":
+    main()
